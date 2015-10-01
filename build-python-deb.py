b1b3758f0c8fde6d17e33fd5fdfddaed3a0ef2d2
Andrey Proskurnev <andrey@proskurnev.ru>
2015-10-01 14:47:44 +0300

    It worked for python-airflow, good

--- build-python-deb.py
+++ build-python-deb.py
@@ -5,6 +5,7 @@ import glob
 import subprocess
 import argparse
 import re
+import os
 
 # sorry for ugly code :(
 
@@ -17,9 +18,25 @@ class Package(object):
             return None
         return m.group(1)
 
-    @classmethod
-    def extract_requests_from_debian_depends(cls, depends):
+    def get_dpkg_info(self):
+        if self.filename is None:
+            return ''
+        with open(os.devnull, "w") as fnull:
+            return subprocess.Popen(["dpkg", "-f", self.filename], stdout=subprocess.PIPE, stderr=fnull).stdout.read()
+        return ''
+
+
+    def extract_requests_from_debian_depends(self):
         result = []
+
+        depends = self.get_dpkg_info() 
+
+        m = re.search('Depends: (.*?)\n', depends, re.S)
+        if m is None:
+            return result
+
+        depends = m.group(1)
+
         if depends is None:
             return result
 
@@ -33,13 +50,29 @@ class Package(object):
             if len(package_and_version) > 1: 
                 version = package_and_version[1]
                 version = version.rstrip()
-                m = re.search('^\s*\(.*?\s*(\d+(\.\d+)*)\s*\)\s*$', version)
+                m = re.search('^\s*\(.*?\s*(\d+(\.\d+)*)(-\d+)?\s*\)\s*$', version)
                 if m is None:
+                    if version is not None and version != '':
+                        self.broken_dependency_number = True
                     version = None
                 else:
                     version = m.group(1)
             result.append(Package(package, version))
         return result
+
+    def get_cached_version(self):
+        output = subprocess.Popen(["apt-cache", "policy", self.name], stdout=subprocess.PIPE).stdout.read()
+        m = re.search('Candidate:\s+(\d+(\.\d+)*)', output, re.S)
+        if m is None:
+            return None
+        return m.group(1)
+
+    def guess_version_from_filename(self):
+        info = self.get_dpkg_info()
+        m = re.search('Version: (.*?)\n', info, re.S)
+        if m is None:
+            return
+        self.version = m.group(1)
 
     @classmethod
     def compare_version(cls, x, y):
@@ -60,28 +93,53 @@ class Package(object):
         return 0
 
 
-    def __init__(self, name, version, filename=None, ignore=None, parents=None):
+    def __init__(self, name, version, filename=None, ignore=None, parents=None, broken=False):
         self.name = name
         self.filename = filename
         self.version = version
         self.ignore = ignore
         self.parents = parents
+        self.broken = broken
+        self.broken_dependency_number = False
+        self.system_version = self.get_cached_version()
+        self.exists_in_system = self.compare_version(self.system_version, self.version) >= 0
         self.is_python = re.match(r'^python-(.*?)$', name) is not None 
+        self.parents = []
 
     def __str__(self):
-        return self.name + '_' + str(self.version)
-        
-    def fpm_arguments(self):
-        if not self.is_python:
-            return ["echo", "not a python package"]
-
-        result = ["fpm", "-s", "python", "-f", "-t", "deb"]
         if self.version is not None:
-            result.append("-v")
-            result.append(self.version)
+            return self.name + '_' + str(self.version)
+        else:
+            return self.name
+        
+    def fpm_build(self, dependencies=None):
+        if not self.is_python:
+            return True
+
+        args = ["fpm", "-s", "python", "-f", "-t", "deb"]
+
+        if self.version is not None:
+            args.append("-v")
+            args.append(self.version)
         fpm_name = re.sub('^python-', '', self.name)
-        result.append(fpm_name)
-        return result
+
+        if dependencies is not None:
+            args.append('--no-auto-depends')
+            for dep in dependencies:
+                args.append('-d')
+                if dep[1] is None:
+                    args.append(dep[0])
+                else:
+                    args.append("%s >= %s" % (dep[0], dep[1]))
+
+        args.append(fpm_name)
+
+        exitcode = 0
+        with open(os.devnull, "w") as fnull:
+            exitcode = subprocess.call(args, stdout=fnull, stderr=None)
+
+        return exitcode == 0 
+
 
 def get_package_version(filename):
     m = re.search('_(.*)\.deb', filename)
@@ -89,54 +147,107 @@ def get_package_version(filename):
         return None
     return m.group(1)
 
-def generate_packages_first_pass(package, ignore):
+def generate_packages_first_pass(package, args):
     result = {}
     to_go = {}
     to_go[package.name] = package
 
     while len(to_go) > 0:
-        print "%i to go %i processed" % (len(to_go), len(result))
+        #print "%i to go %i processed" % (len(to_go), len(result))
 
         _, package = to_go.popitem()
-        print "processing '%s'" % (str(package))
+        result[package.name] = package
+        print "processing %s" % (str(package))
+
+        if package.exists_in_system and not args.force_package_build_over_system:
+            continue
         
         if not package.is_python:
             continue
 
-        subprocess.call(package.fpm_arguments())
-
         files = glob.glob("%s_*.deb" % (package.name))
+        if len(files) == 0 or args.rebuild_existing_package:
+            if not package.fpm_build():
+                package.broken = True
+                continue
+            files = glob.glob("%s_*.deb" % (package.name))
 
         if (len(files) != 1):
-            print "Something wrong, we have several files %s" % (files)
-            continue
+            print "internal error: there should be only one '%s_*.deb' file" % (package.name)
+            print "try to delete all *.deb files and restart script"
+            sys.exit(1)
 
         package.filename = files[0]
+        if package.version is None:
+            package.guess_version_from_filename()
 
-        result[package.name] = package
-
-        depends = subprocess.Popen(["dpkg", "-f", package.filename, "Depends"], stdout=subprocess.PIPE).stdout.read()
-        parents = Package.extract_requests_from_debian_depends(depends)
+        parents = package.extract_requests_from_debian_depends()
+        package.parents = parents
         for parent in parents:
-            if parent.name in ignore:
-                continue
             if parent.name in result and Package.compare_version(result[parent.name].version, parent.version) > 0:
                 continue
             if parent.name in to_go and Package.compare_version(to_go[parent.name].version, parent.version) > 0:
                 continue
             to_go[parent.name] = parent
-    print result
     return result
+
+def optimistic_fix_packages(packages):
+    result = {} 
+    totaly_broken_packages = set() 
+    version_broken_packages = set() 
+    for key, package in packages.iteritems():
+        if not package.broken:
+            continue
+        if package.system_version is not None:
+            print "%s has version problems, remove it's version from dependencies" % (str(package))
+            version_broken_packages.add(package.name)
+        else:
+            print "%s failed to build, remove it from dependencies completely" % (str(package))
+            totaly_broken_packages.add(package.name)
+
+    if len(totaly_broken_packages) + len(version_broken_packages) == 0:
+        return
+
+    # ok we have some broken packages in dependencies, so let's remove them
+    for key, package in packages.iteritems():
+        dependencies = []
+        need_rebuild = False
+
+
+        if package.broken:
+            continue
+
+        if package.broken_dependency_number:
+            need_rebuild = True
+
+        for parent in package.parents:
+            if parent.name in totaly_broken_packages:
+                need_rebuild = True
+                continue
+            if parent.name in version_broken_packages:
+                need_rebuild = True
+                dependencies.append([parent.name, parent.system_version])
+            else:
+                dependencies.append([parent.name, parent.version])
+
+        if not need_rebuild:
+            continue
+
+        print "rebuilding %s" % (str(package))
+        package.fpm_build(dependencies=dependencies)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--package', help='package name', required=True)
     parser.add_argument('--version', help='required package version', default=None, required=True)
-    parser.add_argument('--ignore', help='ignore packages', type=str, nargs='*')
+    parser.add_argument('--force-package-build-over-system', action='store_true')
+    parser.add_argument('--optimistic-ignore-broken-packages', action='store_true')
+    parser.add_argument('--rebuild-existing-package', action='store_true')
     args = parser.parse_args()
-    packages = generate_packages_first_pass(Package(args.package, args.version, ignore=args.ignore), set(args.ignore or []))
-    #subprocess.call(["fpm", "--no-depends", "-s", "python", "-f", "-t", "deb", package])
+    packages = generate_packages_first_pass(Package(args.package, args.version), args)
+    if args.optimistic_ignore_broken_packages:
+        optimistic_fix_packages(packages)
 
 if __name__ == '__main__':
     main()
